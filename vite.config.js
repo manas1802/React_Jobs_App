2cb100a14fbfdfdd8045af8df387326264fe759e
manas1802 <khandelwal1802@gmail.com>
2025-02-14 17:17:09 +0530

    Change

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/React_Jobs_App', // Change this to your actual GitHub repo name
+  base: '/React_Jobs_App/', // Change this to your actual GitHub repo name
   plugins: [react()],
 });
